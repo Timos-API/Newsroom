0074e12bd0d5509876ee4dc2e0aa2e29fc698762
Timo Scheuermann <timo.scheuermann@gmx.de>
2021-07-04 12:42:59 +0200

    added router path prefix

--- news/news.controller.go
+++ news/news.controller.go
@@ -9,13 +9,14 @@ import (
 
 func RegisterRoutes(router *mux.Router) {
 	fmt.Println("News routes registered")
+	s := router.PathPrefix("/newsroom").Subrouter()
 
-	router.HandleFunc("/", getAllNews).Methods("GET")
-	router.HandleFunc("/featured", getFeaturedNews).Methods("GET")
-	router.HandleFunc("/project/{id}", getProjectNews).Methods("GET")
-	router.HandleFunc("/{id}", getNews).Methods("GET")
+	s.HandleFunc("", getAllNews).Methods("GET")
+	s.HandleFunc("/featured", getFeaturedNews).Methods("GET")
+	s.HandleFunc("/project/{id}", getProjectNews).Methods("GET")
+	s.HandleFunc("/{id}", getNews).Methods("GET")
 
-	router.HandleFunc("/", authenticator.AuthMiddleware(postNews, []string{"admin"})).Methods("POST")
-	router.HandleFunc("/{id}", authenticator.AuthMiddleware(deleteNews, []string{"admin"})).Methods("DELETE")
-	router.HandleFunc("/{id}", authenticator.AuthMiddleware(patchNews, []string{"admin"})).Methods("PATCH")
+	s.HandleFunc("", authenticator.AuthMiddleware(postNews, []string{"admin"})).Methods("POST")
+	s.HandleFunc("/{id}", authenticator.AuthMiddleware(deleteNews, []string{"admin"})).Methods("DELETE")
+	s.HandleFunc("/{id}", authenticator.AuthMiddleware(patchNews, []string{"admin"})).Methods("PATCH")
 }
